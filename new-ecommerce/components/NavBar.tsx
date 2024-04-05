ceed2b7d84c2c71cb9986b227966fd5adff701ac
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-05 12:51:25 +0330

    added store svg

--- new-ecommerce/components/NavBar.tsx
+++ new-ecommerce/components/NavBar.tsx
@@ -3,13 +3,16 @@ import { ShoppingCart } from './ShoppingCart'
 import { Avatar } from './Avatar';
 import { SearchBox } from './SearchBox';
 import { MobileDrawer } from './MobileDrawer';
+import { Storefront } from '@phosphor-icons/react/dist/ssr';
 
 export const NavBar = () => {
   return (
     <div>
       <div className="navbar bg-gray-800">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl sm:flex hidden">daisyUI</a>
+          <a className="btn btn-ghost text-xl sm:flex hidden">
+            <Storefront weight='bold' size={25} />
+          </a>
           <div className='sm:hidden'>
             <MobileDrawer/>
           </div>
