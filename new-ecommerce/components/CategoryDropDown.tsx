4c9393c01b852512f95a5d96f80ef184e35428d1
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-05 15:13:03 +0330

    added category dropdown, with icon

---
+++ new-ecommerce/components/CategoryDropDown.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { CaretDown, CaretUp } from "@phosphor-icons/react/dist/ssr";
+import { useState } from "react";
+
+export const CategoryDropDown = () => {
+    const [up, setUp] = useState(false)
+  return (
+    <div>
+      <details className="dropdown">
+        <summary className="m-1 btn" onClick={() => setUp(!up)}>
+            {up ? <CaretUp/> : <CaretDown/> }
+        </summary>
+        <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <a>Item 2</a>
+          </li>
+        </ul>
+      </details>
+    </div>
+  );
+}
