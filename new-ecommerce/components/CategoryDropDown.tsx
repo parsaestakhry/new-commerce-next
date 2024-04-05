a2845939971ff4691d1d8a3c631e51344bd2cf65
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-05 16:12:31 +0330

    removed use effect

--- new-ecommerce/components/CategoryDropDown.tsx
+++ new-ecommerce/components/CategoryDropDown.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import { CaretDown, CaretUp } from "@phosphor-icons/react/dist/ssr";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { CategoryItem } from "./NavBar";
 
-
-export const CategoryDropDown = (props : any) => {
+export const CategoryDropDown = (props: any) => {
   const [up, setUp] = useState(false);
-  const categories = props.categories
- 
+  const categories = props.categories;
 
   return (
     <div>
@@ -21,9 +19,11 @@ export const CategoryDropDown = (props : any) => {
           )}
         </summary>
         <ul className="p-2 shadow menu dropdown-content z-[1] bg-slate-800 rounded-box w-52">
-            {categories.map((item : CategoryItem , index : number) => (
-                <div key={index} className="btn btn-ghost mt-2 text-slate-100">{item.name}</div>
-            ))}
+          {categories.map((item: CategoryItem, index: number) => (
+            <div key={index} className="btn btn-ghost mt-2 text-slate-100">
+              {item.name}
+            </div>
+          ))}
         </ul>
       </details>
     </div>
