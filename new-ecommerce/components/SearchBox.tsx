58f07c468345d7309328c3912b2ca03ccbbcde0f
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-05 12:58:03 +0330

    css

--- new-ecommerce/components/SearchBox.tsx
+++ new-ecommerce/components/SearchBox.tsx
@@ -1,16 +1,16 @@
 import { MagnifyingGlass } from "@phosphor-icons/react/dist/ssr";
 export const SearchBox = () => {
   return (
-    <div className="flex ">
+    <div className="flex">
       {/* for larger displays */}
       <div className="sm:flex hidden">
-        <label className="input input-bordered sm:flex items-center gap-3 ">
+        <label className="input input-bordered sm:flex items-center gap-3 bg-black">
           <input type="text" className="grow" placeholder="Search" />
         </label>
       </div>
       {/* for small displays */}
       <div className="sm:hidden">
-        <label className="input input-bordered flex items-center gap-2 w-36  h-10 mt-1">
+        <label className="input input-bordered flex items-center gap-2 w-36  h-10 mt-1 bg-black">
           <input type="text" className="grow text-sm" placeholder="Search" />
         </label>
       </div>
