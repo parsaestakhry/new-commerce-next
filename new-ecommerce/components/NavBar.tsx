304898fc6fa461e798ced785a8268b32392ca513
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-05 12:38:50 +0330

    added mobile drawer

--- new-ecommerce/components/NavBar.tsx
+++ new-ecommerce/components/NavBar.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { ShoppingCart } from './ShoppingCart'
 import { Avatar } from './Avatar';
 import { SearchBox } from './SearchBox';
+import { MobileDrawer } from './MobileDrawer';
 
 export const NavBar = () => {
   return (
     <div>
       <div className="navbar bg-gray-800">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl sm:flex hidden">daisyUI</a>
+          <div className='sm:hidden'>
+            <MobileDrawer/>
+          </div>
           <div className="mx-auto">
             <SearchBox />
           </div>
