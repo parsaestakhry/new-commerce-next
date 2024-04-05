3c78ed46b61e1e4d133263a2ddda26cb33d4ac49
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-05 15:53:55 +0330

    added category data fetching

--- new-ecommerce/components/NavBar.tsx
+++ new-ecommerce/components/NavBar.tsx
@@ -1,11 +1,33 @@
+'use client'
 import { ShoppingCart } from "./ShoppingCart";
 import { Avatar } from "./Avatar";
 import { SearchBox } from "./SearchBox";
 import { MobileDrawer } from "./MobileDrawer";
 import { Storefront } from "@phosphor-icons/react/dist/ssr";
 import { CategoryDropDown } from "./CategoryDropDown";
+import { useState, useEffect } from "react";
+
+export interface CategoryItem {
+  id: number;
+  name: string;
+  pic: string;
+}
 
 export const NavBar = () => {
+  const [categories, setCategories] = useState<Object>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch("http://127.0.0.1:8000/get-categories/");
+      const data = await response.json();
+      //console.log(data)
+      setCategories(data);
+    };
+    getData();
+    //console.log(categories)
+  }, []);
+
+  //console.log(categories);
   return (
     <div>
       <div className="navbar  bg-slate-700">
@@ -14,7 +36,7 @@ export const NavBar = () => {
             <Storefront weight="bold" size={25} />
           </a>
           <div className="hidden sm:flex">
-            <CategoryDropDown />
+            <CategoryDropDown categories={categories} />
           </div>
 
           <div className="sm:hidden">
