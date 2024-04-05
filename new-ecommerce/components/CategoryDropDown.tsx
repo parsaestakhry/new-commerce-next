162f251c350f2f2a50403bc30233fa5cb3f39a19
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-05 15:30:33 +0330

    fetched categories

--- new-ecommerce/components/CategoryDropDown.tsx
+++ new-ecommerce/components/CategoryDropDown.tsx
@@ -1,15 +1,34 @@
-'use client'
+"use client";
 
 import { CaretDown, CaretUp } from "@phosphor-icons/react/dist/ssr";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const CategoryDropDown = () => {
-    const [up, setUp] = useState(false)
+  const [up, setUp] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch("http://127.0.0.1:8000/get-categories/");
+      const data = await response.json();
+      //console.log(data)
+      setCategories(data);
+    };
+    getData();
+    //console.log(categories)
+  }, []);
+
+  //console.log(categories);
+
   return (
     <div>
       <details className="dropdown">
         <summary className="m-1 btn btn-ghost" onClick={() => setUp(!up)}>
-            {up ? <CaretUp size={20} weight="bold"/> : <CaretDown size={20} weight="bold"/> }
+          {up ? (
+            <CaretUp size={20} weight="bold" />
+          ) : (
+            <CaretDown size={20} weight="bold" />
+          )}
         </summary>
         <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
           <li>
@@ -22,4 +41,4 @@ export const CategoryDropDown = () => {
       </details>
     </div>
   );
-}
+};
