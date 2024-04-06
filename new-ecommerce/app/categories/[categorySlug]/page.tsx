483837ebdbb3598bc977cbd92d58637205cd6604
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-06 09:44:34 +0330

    fetched data from api

--- new-ecommerce/app/categories/[categorySlug]/page.tsx
+++ new-ecommerce/app/categories/[categorySlug]/page.tsx
@@ -1,8 +1,21 @@
+"use client";
+import { useEffect, useState } from "react";
 
-const page = ({params} : {params : {categorySlug : string}}) => {
-  return (
-    <div>{params.categorySlug}</div>
-  )
-}
+const page = ({ params }: { params: { categorySlug: string } }) => {
+  const [products, setProducts] = useState([]);
 
-export default page
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(`http://127.0.0.1:8000/get-products/`);
+      const data = await response.json();
+      console.log(data);
+    //   console.log("hello");
+    
+    };
+    getData();
+  }, []);
+
+  return <div>{params.categorySlug}</div>;
+};
+
+export default page;
