08a735dfea33b29aba9a0e84f87aabd66f89607b
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-06 09:37:52 +0330

    implemented router push for categories

--- new-ecommerce/components/CategoryDropDown.tsx
+++ new-ecommerce/components/CategoryDropDown.tsx
@@ -3,10 +3,12 @@
 import { CaretDown, CaretUp } from "@phosphor-icons/react/dist/ssr";
 import { useState } from "react";
 import { CategoryItem } from "./NavBar";
+import { useRouter } from "next/navigation";
 
 export const CategoryDropDown = (props: any) => {
   const [up, setUp] = useState(false);
   const categories = props.categories;
+  const router = useRouter();
 
   return (
     <div>
@@ -20,7 +22,11 @@ export const CategoryDropDown = (props: any) => {
         </summary>
         <ul className="p-2 shadow menu dropdown-content z-[1] bg-slate-800 rounded-box w-52">
           {categories.map((item: CategoryItem, index: number) => (
-            <div key={index} className="btn btn-ghost mt-2 text-slate-100">
+            <div
+              key={index}
+              className="btn btn-ghost mt-2 text-slate-100"
+              onClick={() => router.push(`/categories/${item.name}`)}
+            >
               {item.name}
             </div>
           ))}
