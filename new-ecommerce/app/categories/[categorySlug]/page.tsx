07235ad8611f0dd566097af9334051953eb427c2
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-06 09:50:43 +0330

    created product interface

--- new-ecommerce/app/categories/[categorySlug]/page.tsx
+++ new-ecommerce/app/categories/[categorySlug]/page.tsx
@@ -1,5 +1,18 @@
 "use client";
+import { CategoryItem } from "@/components/NavBar";
 import { useEffect, useState } from "react";
+
+export interface Product {
+  amount: number;
+  category: string;
+  date_created: string;
+  description: string;
+  id: number;
+  name: string;
+  pic: string;
+  price: number;
+  purchase: Array<number>;
+}
 
 const page = ({ params }: { params: { categorySlug: string } }) => {
   const [products, setProducts] = useState([]);
@@ -9,8 +22,7 @@ const page = ({ params }: { params: { categorySlug: string } }) => {
       const response = await fetch(`http://127.0.0.1:8000/get-products/`);
       const data = await response.json();
       console.log(data);
-    //   console.log("hello");
-    
+      //   console.log("hello");
     };
     getData();
   }, []);
