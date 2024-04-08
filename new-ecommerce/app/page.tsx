933e6eaf5a11890ab853bedfe64ce4b5acb91c80
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-08 18:12:22 +0330

    for small devices

--- new-ecommerce/app/page.tsx
+++ new-ecommerce/app/page.tsx
@@ -12,6 +12,8 @@ export interface Product {
   amount: number;
   description: string;
   purchase: Array<number>;
+  name: string;
+  pic: string;
 }
 
 export default function Home() {
@@ -22,7 +24,7 @@ export default function Home() {
       const response = await fetch(`http://127.0.0.1:8000/get-products/`);
       const data = await response.json();
       //console.log(data);
-      setProducts(data)
+      setProducts(data);
     };
     getProducts();
   }, []);
@@ -31,9 +33,17 @@ export default function Home() {
 
   return (
     <>
-      {products.map((item : Product, index : number) => (
-        <ProductCard item={item} key={index} />
-      ))}
+      <div className="sm:flex space-x-2  justify-center hidden flex-wrap">
+        {products.map((item: Product, index: number) => (
+          <ProductCard item={item} key={index} />
+        ))}
+      </div>
+
+      <div className="sm:hidden flex flex-wrap justify-center">
+        {products.map((item: Product, index: number) => (
+          <ProductCard item={item} key={index} />
+        ))}
+      </div>
     </>
   );
 }
