1df9d94be37a643547502f2b2e2eefac64674f83
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-09 00:01:17 +0330

    fetching products by category

--- new-ecommerce/app/categories/[categorySlug]/page.tsx
+++ new-ecommerce/app/categories/[categorySlug]/page.tsx
@@ -1,38 +1,26 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
 
 const Page = ({ params }: { params: { categorySlug: string } }) => {
   const [products, setProducts] = useState([]);
-  const [categoryId, setCategoryId] = useState<number>();
 
   useEffect(() => {
-    const getId = async () => {
+    const getCategoryIdProducts = async () => {
       const response = await fetch(
         `http://127.0.0.1:8000/get-category-id/${params.categorySlug}/`
       );
       const data = await response.json();
-      setCategoryId(data);
+      //console.log(data)
+      setProducts(data);
     };
-    getId();
+    getCategoryIdProducts();
   }, [params.categorySlug]); // Trigger when categorySlug changes
 
-  useEffect(() => {
-    const getData = async () => {
-      if (categoryId !== undefined) {
-        // Check if categoryId is defined
-        const response = await fetch(
-          `http://127.0.0.1:8000/get-category-products/?category=${categoryId}`
-        );
-        const data = await response.json();
-        setProducts(data);
-      }
-    };
-    getData();
-  }, [categoryId]); // Trigger when categoryId changes
+  // Trigger when categoryId changes
 
   // console.log(categoryId);
-  // console.log(products);
+  //console.log(products);
 
   return <div></div>;
 };
