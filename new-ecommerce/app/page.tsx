a797be7a897984d6db27758a82dad890ae22e3ee
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-08 17:48:10 +0330

    fetched products from api

--- new-ecommerce/app/page.tsx
+++ new-ecommerce/app/page.tsx
@@ -1,5 +1,37 @@
+"use client";
+
+import { ProductCard } from "@/components/ProductCard";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export interface Product {
+  id: number;
+  category: string;
+  price: number;
+  date_created: Date;
+  amount: number;
+  description: string;
+  purchase: Array<number>;
+}
+
 export default function Home() {
-  return <>
-  </>;
+  const [products, setProducts] = useState<Product | null>();
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch(`http://127.0.0.1:8000/get-products/`);
+      const data = await response.json();
+      //console.log(data);
+      setProducts(data)
+    };
+    getProducts();
+  }, []);
+
+  //console.log(products)
+
+  return (
+    <>
+      <ProductCard />
+    </>
+  );
 }
