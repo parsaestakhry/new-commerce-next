22b03e3f0aa2f6880003b4c89b35491e99429252
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-11 11:20:35 +0330

    added user icon

--- new-ecommerce/components/Avatar.tsx
+++ new-ecommerce/components/Avatar.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { User } from "@phosphor-icons/react/dist/ssr";
+import React from "react";
 
 export const Avatar = () => {
   return (
@@ -9,16 +10,13 @@ export const Avatar = () => {
           role="button"
           className="btn btn-ghost btn-circle avatar"
         >
-          <div className="w-10 rounded-full">
-            <img
-              alt="Tailwind CSS Navbar component"
-              src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-            />
+          <div className="w-10 rounded-full p-1">
+            <User size={30} weight="bold" />
           </div>
         </div>
         <ul
           tabIndex={0}
-          className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-black rounded-box w-52"
         >
           <li>
             <a className="justify-between">
@@ -26,9 +24,7 @@ export const Avatar = () => {
               <span className="badge">New</span>
             </a>
           </li>
-          <li>
-            <a>Settings</a>
-          </li>
+
           <li>
             <a>Logout</a>
           </li>
@@ -36,4 +32,4 @@ export const Avatar = () => {
       </div>
     </div>
   );
-}
+};
