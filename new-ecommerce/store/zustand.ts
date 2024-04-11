56491505983fa095b47be0f40ff3bb54201e851c
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-11 14:05:56 +0330

    added dev tools

--- new-ecommerce/store/zustand.ts
+++ new-ecommerce/store/zustand.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { mountStoreDevtool } from "simple-zustand-devtools";
 
 type CartStore = {
   cart: number;
@@ -7,7 +8,6 @@ type CartStore = {
   removeAll: () => void;
 };
 
-
 export const useCartStore = create<CartStore>((set) => ({
   cart: 0,
   add: () => set((state) => ({ cart: state.cart + 1 })),
@@ -15,11 +15,15 @@ export const useCartStore = create<CartStore>((set) => ({
   removeAll: () => set({ cart: 0 }),
 }));
 
-
 export const useCategoryStore = create((set) => ({
-    categories : [],
-    fetch : async () => {
-        const response = await fetch("http://127.0.0.1:8000/get-categories/");
-        set({categories : await response.json()})
-    }
-}))
+  categories: [],
+  fetch: async () => {
+    const response = await fetch("http://127.0.0.1:8000/get-categories/");
+    set({ categories: await response.json() });
+  },
+}));
+
+if (process.env.NODE_ENV === "development") {
+  mountStoreDevtool("Store", useCartStore);
+  mountStoreDevtool("category", useCategoryStore);
+}
