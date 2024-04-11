6d9378525d476e8c0af201fc5f1a3a71dccd85e8
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-11 16:39:22 +0330

    login artboard

---
+++ new-ecommerce/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginArtBoard } from "@/components/LoginArtBoard"
+
+const page = () => {
+  return (
+    <div className="flex justify-center"><LoginArtBoard/></div>
+  )
+}
+
+export default page
