b0d9c87ecf5d8d6d509f7ecbd40d971343593a2c
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-18 12:25:30 +0330

    console logged the token

--- new-ecommerce/app/login/user/page.tsx
+++ new-ecommerce/app/login/user/page.tsx
@@ -3,27 +3,8 @@ import axios from "axios";
 import React from "react";
 
 const page = () => {
-  const handleRequest = async () => {
-    try {
-      // Set axios defaults for credentials
-      axios.defaults.withCredentials = true;
-
-      const response = await axios.get("http://127.0.0.1:8000/get-user-list/");
-      console.log(response);
-
-      // Assuming the backend sends cookies in the response
-      // You can extract and store these cookies if needed
-      console.log("Login successful", response.data);
-    } catch (error) {
-      console.error("Login failed", error);
-    }
-  };
-
-  return (
-    <div className="btn btn-primary" onClick={() => handleRequest()}>
-      click me
-    </div>
-  );
+  //console.log(localStorage.getItem('token'))
+  return <div className="btn btn-primary">click me</div>;
 };
 
 export default page;
