7a975cebf5b49ced7b428a0f91b80659a3703842
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-20 09:47:59 +0330

    removed token log

--- new-ecommerce/app/login/user/page.tsx
+++ new-ecommerce/app/login/user/page.tsx
@@ -3,8 +3,7 @@ import axios from "axios";
 import React from "react";
 
 const page = () => {
-  const token = localStorage.getItem('token')
-  console.log(token)
+  
   return <div className="btn btn-primary">click me</div>;
 };
 
