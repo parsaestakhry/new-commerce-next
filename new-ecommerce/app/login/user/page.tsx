4602318d141a2e6e33c5d22716aaa523efea79f7
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-20 22:23:33 +0330

    product fetched

--- new-ecommerce/app/login/user/page.tsx
+++ new-ecommerce/app/login/user/page.tsx
@@ -1,10 +1,35 @@
 "use client";
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
-  
-  return <div className="btn btn-primary">click me</div>;
+  const [products, setProducts] = useState({});
+
+  const local_token = localStorage.getItem("token");
+  // console.log(local_token)
+
+  useEffect(() => {
+    const getUserProducts = async () => {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/get-user-list/",
+        {
+          token: local_token,
+        }
+      );
+
+      const data = response.data;
+      //console.log(data)
+      setProducts(data);
+    };
+    getUserProducts();
+  }, []);
+
+  console.log(products);
+  return (
+    <div className="btn btn-primary" >
+      click me
+    </div>
+  );
 };
 
 export default page;
