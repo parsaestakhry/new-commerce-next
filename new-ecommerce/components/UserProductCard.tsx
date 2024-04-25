24532e90f2541457a8a19d966f892ca43c9946d4
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-25 17:45:52 +0330

    added userproduct Card

---
+++ new-ecommerce/components/UserProductCard.tsx
@@ -0,0 +1,56 @@
+import { Product } from "@/app/page";
+import Image from "next/image";
+
+export const UserProductCard = ({ item }: { item: Product }) => {
+  const local = "http://127.0.0.1:8000/";
+  return (
+    <div>
+      <div className="hidden sm:flex">
+        <div className="card w-80 bg-slate-700 shadow-xl mx-2">
+          <figure>
+            <Image
+              src={local + item.pic}
+              alt="Shoes"
+              width={400}
+              height={400}
+              className="object-cover h-56 w-80"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              {item.name}
+              <div className="badge badge-secondary">{item.category}</div>
+            </h2>
+            <p>{item.description}</p>
+          </div>
+          <button className="btn btn-ghost mb-2 w-24 mx-auto">
+            Check Out
+          </button>
+        </div>
+      </div>
+
+      {/* for smaller displays */}
+      <div className="sm:hidden">
+        <div className="card w-80 bg-slate-700 shadow-xl mx-2 mt-4 mb-4">
+          <figure>
+            <Image
+              src={local + item.pic}
+              alt="Shoes"
+              width={400}
+              height={400}
+              className="object-cover h-56 w-80"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              {item.name}
+              <div className="badge badge-secondary">{item.category}</div>
+            </h2>
+            <p>{item.description}</p>
+          </div>
+          <button className="btn btn-ghost mb-2 w-24 mx-auto">Check out</button>
+        </div>
+      </div>
+    </div>
+  );
+};
