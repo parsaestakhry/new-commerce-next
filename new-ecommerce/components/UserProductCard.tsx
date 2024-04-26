4a7627cc51aac92bf9a72adb04bad292c91483f4
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-26 19:23:23 +0330

    purchase instead of product

--- new-ecommerce/components/UserProductCard.tsx
+++ new-ecommerce/components/UserProductCard.tsx
@@ -1,10 +1,13 @@
+import { Purchase } from "@/app/login/user/page";
 import { Product } from "@/app/page";
 import axios from "axios";
 import Image from "next/image";
-import { it } from "node:test";
+import { useEffect, useState } from "react";
 
-export const UserProductCard = ({ item }: { item: Product }) => {
+export const UserProductCard = ({ item }: { item: Purchase }) => {
   const local = "http://127.0.0.1:8000/";
+  const [product, setProduct] = useState<Product | null>(null); // Define type as Product | null
+
   const handleRemoveCart = async () => {
     const response = await axios.delete(
       `http://127.0.0.1:8000/remove-from-list/${item.id}/`,
@@ -20,13 +23,27 @@ export const UserProductCard = ({ item }: { item: Product }) => {
 
     window.location.reload();
   };
+
+  useEffect(() => {
+    const getProduct = async () => {
+      const response = await axios.get(
+        `http://127.0.0.1:8000/get-product/${item.product_id}/`
+      );
+      const data: Product = await response.data; // Define data as Product
+      setProduct(data);
+    };
+    getProduct();
+  }, []);
+
+  if (!product) return null; // Handling null case to avoid rendering errors
+
   return (
     <div>
       <div className="hidden sm:flex">
         <div className="card w-80 bg-slate-700 shadow-xl mx-2">
           <figure>
             <Image
-              src={local + item.pic}
+              src={local + product.pic}
               alt="Shoes"
               width={400}
               height={400}
@@ -35,10 +52,10 @@ export const UserProductCard = ({ item }: { item: Product }) => {
           </figure>
           <div className="card-body">
             <h2 className="card-title">
-              {item.name}
-              <div className="badge badge-secondary">{item.category}</div>
+              {product.name}
+              <div className="badge badge-secondary">{product.category}</div>
             </h2>
-            <p>{item.description}</p>
+            <p>{product.description}</p>
           </div>
           <div className="flex">
             <button className="btn btn-ghost mb-2 w-24 mx-auto">
@@ -59,7 +76,7 @@ export const UserProductCard = ({ item }: { item: Product }) => {
         <div className="card w-80 bg-slate-700 shadow-xl mx-2 mt-4 mb-4">
           <figure>
             <Image
-              src={local + item.pic}
+              src={local + product.pic}
               alt="Shoes"
               width={400}
               height={400}
@@ -68,10 +85,10 @@ export const UserProductCard = ({ item }: { item: Product }) => {
           </figure>
           <div className="card-body">
             <h2 className="card-title">
-              {item.name}
-              <div className="badge badge-secondary">{item.category}</div>
+              {product.name}
+              <div className="badge badge-secondary">{product.category}</div>
             </h2>
-            <p>{item.description}</p>
+            <p>{product.description}</p>
           </div>
           <button className="btn btn-ghost mb-2 w-24 mx-auto">Check out</button>
           <button className="btn btn-ghost mb-2 w-24 mx-auto">Check out</button>
