d4e3a701bf79e84820d86afe22cf35d79a8742db
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-27 09:05:30 +0330

    using the amount in cart component

--- new-ecommerce/components/ShoppingCart.tsx
+++ new-ecommerce/components/ShoppingCart.tsx
@@ -1,6 +1,8 @@
+import { useAmountStore } from '@/store/zustand';
 import React from 'react'
 
 export const ShoppingCart = () => {
+  const count = useAmountStore((state) => state.count)
   return (
     <div>
       <div className="dropdown dropdown-end">
@@ -20,7 +22,7 @@ export const ShoppingCart = () => {
                 d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
               />
             </svg>
-            <span className="badge badge-sm indicator-item">8</span>
+            <span className="badge badge-sm indicator-item">{count}</span>
           </div>
         </div>
         <div
