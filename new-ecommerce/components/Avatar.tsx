23b9b9e0b6c1478ae35612432caecc2d440bb9c7
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-29 15:19:45 +0330

    changed avatar to a dropdown

--- new-ecommerce/components/Avatar.tsx
+++ new-ecommerce/components/Avatar.tsx
@@ -1,32 +1,24 @@
+import { useInfoStore } from "@/store/zustand";
 import { User } from "@phosphor-icons/react/dist/ssr";
 import React from "react";
 
 export const Avatar = () => {
+  const username = localStorage.getItem('username')
   return (
     <div>
       <div className="dropdown dropdown-end">
-        <div
-          tabIndex={0}
-          role="button"
-          className="btn btn-ghost btn-circle avatar"
-        >
-          <div className="w-10 rounded-full p-1">
-            <User size={30} weight="bold" />
-          </div>
+        <div tabIndex={0} role="button" className="btn m-1 bg-purple-400 text-gray-950 ml-3 hover:bg-purple-900">
+          welcome {username}
         </div>
         <ul
           tabIndex={0}
-          className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-black rounded-box w-52"
+          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
         >
           <li>
-            <a className="justify-between">
-              Profile
-              <span className="badge">New</span>
-            </a>
+            <a>Item 1</a>
           </li>
-
           <li>
-            <a>Logout</a>
+            <a>Item 2</a>
           </li>
         </ul>
       </div>
