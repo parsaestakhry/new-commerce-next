965b3eeb40b82f7953344be6541b6cc9a5fe6acf
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-29 17:44:33 +0330

    fetched product

--- new-ecommerce/app/products/[productId]/page.tsx
+++ new-ecommerce/app/products/[productId]/page.tsx
@@ -1,11 +1,22 @@
-const page = ({params} : {params : {productId : number}}) => {
-    const product_id = params.productId
-    
-  return (
-    <div>
-        
-    </div>
-  )
-}
+"use client";
 
-export default page
+import { Product } from "@/app/page";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const page = ({ params }: { params: { productId: number } }) => {
+  const product_id = params.productId;
+  const [product, setProduct] = useState<Product | null>(null)
+  useEffect(() => {
+    const getProduct = async() => {
+      const response = await axios.get(`http://127.0.0.1:8000/get-product/${product_id}/`);
+      setProduct(await response.data)
+    }
+    getProduct();
+  }, [])
+  
+
+  return <div></div>;
+};
+
+export default page;
