adff252ca9eb2e5c5e14d88c51c7eaf363e23f8f
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-01 19:28:38 +0330

    commit

--- new-ecommerce/components/ProductBoard.tsx
+++ new-ecommerce/components/ProductBoard.tsx
@@ -1,19 +1,37 @@
 import { Product } from "@/app/page";
-export const ProductBoard = (item : any) => {
+import { useRouter } from "next/navigation";
+import { it } from "node:test";
+import axios from "axios";
+export const ProductBoard = (item: any) => {
+  const local = "http://127.0.0.1:8000/";
+  const router = useRouter();
+  const handleAddCart = async () => {
+    const response = await axios.post(
+      `http://127.0.0.1:8000/add-multiple-list/${item.item.id}/1/`,
+      {
+        token: localStorage.getItem("token"),
+      }
+    );
+    console.log(response);
+
+    setTimeout(() => window.location.reload(), 200);
+  };
+  
   return (
     <div>
-      <div className="card lg:card-side bg-black shadow-xl">
+      <div className="card lg:card-side bg-black shadow-xl min-h-lvh">
         <figure>
           <img
-            src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+            className="object-cover"
+            src={local + item.item.pic}
             alt="Album"
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{item.item.name}</h2>
-          <p>{item.item.description}</p>
+          <h2 className="card-title  text-4xl">{item.item.name}</h2>
+          <p className="text-3xl">{item.item.description}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">add to cart</button>
+            <button className="btn btn-primary" onClick={() => handleAddCart()}>add to cart</button>
             <button className="btn btn-primary">Buy Now</button>
           </div>
         </div>
