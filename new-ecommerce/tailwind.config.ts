197e5f6a0d7885b4436d876b7be526f9ee0bf0ce
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-05-02 14:27:10 +0330

    added colors

--- new-ecommerce/tailwind.config.ts
+++ new-ecommerce/tailwind.config.ts
@@ -6,6 +6,21 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "resolution-blue": {
+        "50": "#e5f4ff",
+        "100": "#d0ebff",
+        "200": "#abd8ff",
+        "300": "#79bbff",
+        "400": "#448dff",
+        "500": "#1b5eff",
+        "600": "#003eff",
+        "700": "#0041ff",
+        "800": "#0039de",
+        "900": "#03238c",
+        "950": "#031763",
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -14,6 +29,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('daisyui')],
+  plugins: [require("daisyui")],
 };
 export default config;
